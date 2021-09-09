bdd055f2c74a1aa1a9f570db12da4483f093d92e
Martin Lenart <martin_lenart@icloud.com>
2021-09-09 12:52:03 +0200

    Exercise update

--- BOOP_03_10/Program.cs
+++ BOOP_03_10/Program.cs
@@ -10,8 +10,29 @@ namespace BOOP_03_10
 			public int X, Y;
 			public double Vector => Math.Sqrt(X * X + Y * Y);
 		}
+		public enum PlayingCardColor
+		{
+			Heart = 0b1,
+			Diamond = 0b1 << 1,
+			Club = 0b1 << 2,
+			Spade = 0b1 << 3,
+
+			Red = Heart | Diamond,
+			Black = Club | Spade
+		}
+		public enum PlayingCardValue
+		{
+			Ace = 1,
+			One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+			Knight, Queen, King
+		}
+		public struct PlayingCard
+		{
+			public PlayingCardColor Color;
+			public PlayingCardValue Value;
+		}
 		#endregion
-		
+
 		static void Main(string[] args)
         {
 			// Array initialization
@@ -55,3 +76,8 @@ namespace BOOP_03_10
 // 2.   Make a 1-dimensionalan array of struct Point (from region Exercise), initialize using Simple syntax way.
 //		Tips: To initialize each element in the array you use (you can set X and Y to different values): new Point { X = 0, Y = 0 }
 // 3.	Iterate over the array of type Point and print out all the points.
+// 4.	Make an array of type PlayingCard, call the variable DeckOfCards. Initialize the DeckOfCards with 4 playing cards, Ace of Spade, Ace of Club,
+//		Ace of Heart, and Ace of Diamonds
+
+// 5.	Challange: Write code to initialize a complete deck of Card with 52 cards, using two nested for-loops.
+//		The outer loop, the color of the card, the inner loop the value of the card.  Hint: Exercise BOOP_03_04.
