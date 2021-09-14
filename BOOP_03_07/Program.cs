413c155a6c06dc64f72b4ae560dc1107f34ade62
Martin Lenart <martin_lenart@icloud.com>
2021-09-14 14:20:47 +0200

    Exercise Update

--- BOOP_03_07/Program.cs
+++ BOOP_03_07/Program.cs
@@ -61,6 +61,8 @@ namespace BOOP_03_07
 // Exercise:
 // 1.   Create an array of int that is 10 elements in size. Initialize it using a for-loop were each element has value index*2
 // 2.   Create another array of int that is 10 elements in size. Initialize it using a foreach-loop were each element has value index*2
+// 2a.  Iterate over the int array using a foreach-loop and find the largest, and smallest integer. Print them out.
+
 // 3.   Make an array of struct Point, initialize it with 10 different Point cooridinates.
 //      Iterate over it using a foreach-loop and find the longest Vector. Print out the longest Vector.  
 // 4.   Modify the code in 1. to find also the shortest Vector. Printout both the longest and the shortest vectors.
