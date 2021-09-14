69d26796f90fc2e33195c79f2eaa5bef3798d1a1
Martin Lenart <martin_lenart@icloud.com>
2021-09-14 12:14:04 +0200

    Exercise Update

---
+++ Excercise 3_03_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Excercise_3_03_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"i: {i++}");
+
+                int k = 0;
+                while (k < 10)
+                {
+                    Console.WriteLine($"k: {k++}");
+                }
+            }
+        }
+    }
+}
---
+++ Exercise 3_03_02a/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise_3_03_02a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine($"i: {i}");
+                for (int k = 1; k <= 10; k++)
+                {
+                    Console.WriteLine($"k: {k}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+//2.	Write two nested for-loops (a for-loop in a for-loop). The outer loop counts down from 50 to 0, and for each count down the 
+//		inner loop counts up 1..10. Print out in below form:
+//		50: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+//		49: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+//		...
+//		0: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
